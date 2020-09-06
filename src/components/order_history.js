4c4c67acc19d621a245657e2a5359bbff36a9dc5
root <root@localhost.localdomain>
2020-09-06 13:43:11 +0530

    order history

--- src/components/order_history.js
+++ src/components/order_history.js
@@ -1,12 +1,13 @@
 import React,{Component} from 'react'
 // import Post from '../templates/post'
-
+import './order_history.scss'
 class Order extends Component{
     constructor(props) {
         super(props)
         
         this.state = {
              posts:[],
+             orders:[],
              isLoaded:false
         }
         // this.loadposts=this.loadposts.bind(this)
@@ -14,23 +15,14 @@ class Order extends Component{
     
     componentDidMount()
     {
-        // console.log(this)
-       const _this=this
-        // fetch('https://jsonplaceholder.typicode.com/todos/2')
-        fetch('https://jsonplaceholder.typicode.com/users')
-        .then(res=> res.json())
+        fetch('http://178.128.90.226:8000/orders')
+        .then(res=>{return res.json()})
         .then(resData=>{
-            // console.log(resData)
-            // const post_arr=resData
-            // console.log(post_arr)
-            _this.setState({
-                posts:resData,
-                isLoaded:true
+            console.log(resData)
+            this.setState({
+                orders:resData,
+                is_Loading:true
             })
-            console.log(this.state.posts)
-        })
-        .catch(err=>{
-            console.log(err)
         })
     }
 
@@ -38,16 +30,51 @@ class Order extends Component{
     {
         const {posts,isLoaded}=this.state
         return (
-            <div>
-                <div>
-                    <h1>Order History</h1>
-                </div>
-                {/* {
-                    posts.map(post=>{
-                       return  <Post key={post.id} name={post.name} email={post.email} />
-                    })
-                } */}
-            </div>
+           
+            <section class="md-ui component-data-table">
+	<header class="main-table-header">
+		<h1 class="table-header--title">Order History</h1>
+		<span class="table-header--icons"><i class="material-icons">filter_list</i><i class="material-icons">more_vert</i>
+		</span>
+	</header>
+	<div class="main-table-wrapper">
+		<table class="main-table-content">
+			<thead class="data-table-header">
+				<tr class="data-table-row">
+                    <td class="table-datacell datatype-numeric">Order ID</td>
+					<td class="table-datacell datatype-string">Customer</td>
+					<td class="table-datacell datatype-numeric">Total Price</td>
+                    <td class="table-datacell datatype-string">Order Date</td>
+				</tr>
+			</thead>
+            
+			<tbody class="data-table-content">
+            {this.state.orders.map( order => (
+                
+			<tr class="data-table-row">
+			<td class="table-datacell datatype-numeric"><a href={'/order/' + order.id}>{order.id}</a></td>
+            <td class="table-datacell datatype-string">{order.customer.customer_name}</td>
+            <td class="table-datacell datatype-numeric">{order.total_price}</td>
+            <td class="table-datacell datatype-string">{order.date}</td>
+           
+                </tr>
+                
+				))}
+			</tbody>
+		</table>
+	</div>
+	<footer class="main-table-footer">
+		<span class="rows-selection">
+			<span class="rows-selection-label">Rows per page:</span>
+			<span class="rows-selection-dropdown">10<i class="material-icons">arrow_drop_down</i></span>
+		</span>
+		<span class="rows-amount">1-10 of 100</span>
+		<span class="table-pagination">
+			<i class="material-icons">keyboard_arrow_left</i>
+			<i class="material-icons">keyboard_arrow_right</i>
+		</span>
+	</footer>
+</section>
 
         )
     }
